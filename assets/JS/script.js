5f273da235d3fdd07496eaa74c2a2c14ac3ed79e
Mr. A <ag.perch@gmail.com>
2019-12-27 11:47:00 -0800

    Links will hide and show the pages

---
+++ assets/JS/script.js
@@ -0,0 +1,34 @@
+const about=$(".about")
+const contact=$(".contact")
+const portfolio=$(".portfolio")
+const aboutClick=$(".about-click")
+const conatactClick=$(".contact-click")
+const portfolioClick=$(".portfolio-click")
+
+
+portfolio.hide()
+contact.hide()
+portfolioClick.click(function(event){
+    event.preventDefault()
+    portfolio.show()
+    about.hide()
+    contact.hide()
+})
+conatactClick.click(function(event){
+    event.preventDefault()
+    contact.show()
+    about.hide()
+    portfolio.hide()
+})
+aboutClick.click(function(event){
+    event.preventDefault()
+    about.show()
+    portfolio.hide()
+    contact.hide()
+})
+$("button").click(function(event){
+    event.preventDefault()
+    alert("This fuction is not operational yet, sorry for the inconvenience")
+})
+
+
